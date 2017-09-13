614714c853b885c27eace932d65f7faa7fe53bca
Daniel <daniel-yim@live.com>
2017-09-13 14:05:58 +0800

    添加全局安装脚本

--- inserter.sh
+++ inserter.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+#初始化参数
+EXEC_BASE=$(cd `dirname $0`; pwd)
+
 # 自动安装
 
 function download()
 {
     echo -n "Begin Download...."
 }
+
+#注册全局
+ln -s $EXEC_BASE/bin/tomcat-cli.sh /usr/local/bin/tomcat-cli
